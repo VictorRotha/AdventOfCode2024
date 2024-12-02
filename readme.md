8de189e3bb5474edd92b8c7fe51b02977e83d086
Victor Rotha <victor.rotha@t-online.de>
2024-12-02 08:22:34 +0100

    day02

--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # [Advent of Code 2024](https://adventofcode.com/2024)
 
 - [Day 1: Historian Hysteria](https://github.com/VictorRotha/AdventOfCode2024/blob/master/src/main/kotlin/day01/Day01.kt)
+- [Day 2: Red-Nosed Reports](https://github.com/VictorRotha/AdventOfCode2024/blob/master/src/main/kotlin/day02/Day02.kt)
---
+++ src/main/kotlin/day02/Day02.kt
@@ -0,0 +1,86 @@
+package day02
+
+import java.io.File
+import kotlin.math.abs
+
+fun main() {
+
+    val file = File("src/main/kotlin/day02/input.txt")
+
+    part01(file)
+    part02(file)
+
+
+}
+
+fun part01(file: File) {
+    var count = 0
+    file.bufferedReader().forEachLine { line ->
+        val report = line.split(" ").map { it.toInt() }
+
+        if (isSafeReport(report))
+            count++
+
+
+
+    }
+
+
+    println("Solution part 01: $count")
+}
+
+
+fun part02(file: File) {
+    var count = 0
+    file.bufferedReader().forEachLine { line ->
+        val report = line.split(" ").map { it.toInt() }
+
+        if (isSafeReport(report))
+            count++
+        else {
+            for (i in report.indices) {
+                val altered = report.filterIndexed { index, j ->
+                    index != i
+                }
+                if (isSafeReport(altered)) {
+                    count++
+                    break
+                }
+            }
+        }
+    }
+
+    println("Solution part 02: $count")
+
+}
+
+fun isSafeReport(report: List<Int>) : Boolean {
+
+    var dir = 0
+
+    for (i in 1..<report.size) {
+
+        val delta = report[i] - report[i - 1]
+        if (!isSafeStep(delta, dir)) {
+                return false
+        }
+
+        dir = if (delta > 0) 1 else -1
+
+    }
+
+    return true
+}
+
+fun isSafeStep(delta : Int, dir: Int) : Boolean {
+
+    if (delta == 0 || abs(delta) > 3) {
+        return false
+    } else if (delta > 0 && dir < 0) {
+            return false
+    } else if (delta < 0 && dir > 0) {
+            return false
+    }
+    return true
+
+}
