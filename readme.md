29f6aadbc23be72d8672c13de4d9edd1bf703ba8
Victor Rotha <victor.rotha@t-online.de>
2024-12-05 11:34:43 +0100

    day05 part02

--- readme.md
+++ readme.md
@@ -4,3 +4,4 @@
 - [Day 2: Red-Nosed Reports](https://github.com/VictorRotha/AdventOfCode2024/blob/master/src/main/kotlin/day02/Day02.kt)
 - [Day 3: Mull It Over](https://github.com/VictorRotha/AdventOfCode2024/blob/master/src/main/kotlin/day03/Day03.kt)
 - [Day 4: Ceres Search](https://github.com/VictorRotha/AdventOfCode2024/blob/master/src/main/kotlin/day04/Day04.kt)
+- [Day 5: Print Queue](https://github.com/VictorRotha/AdventOfCode2024/blob/master/src/main/kotlin/day05/Day05.kt)
--- src/main/kotlin/day05/Day05.kt
+++ src/main/kotlin/day05/Day05.kt
@@ -8,6 +8,7 @@ fun main() {
 
     val rules = mutableMapOf<Int, MutableList<Int>>()
     val correctNumbers = mutableListOf<List<Int>>()
+    val correctedNumbers = mutableListOf<List<Int>>()
     var readRules = true
     file.bufferedReader().forEachLine {line ->
 
@@ -24,24 +25,32 @@ fun main() {
             rules[key]?.add(rule[1].toInt())
 
         } else {
-            val numbers = line.split(",").map {it.toInt()}
-            var correct = true
-            for (i in 0 ..< numbers.size - 1) {
-                if (rules.containsKey(numbers[i + 1]) && numbers[i] in rules[numbers[i + 1]]!!) {
-                    correct = false
-                    break
+            val numbers = line.split(",").map {it.toInt()}.toMutableList()
+            var correct = false
+            var corrected = false
+            while (!correct) {
+                correct = true
+                for (i in 0 ..< numbers.size - 1) {
+                    if (rules.containsKey(numbers[i + 1]) && numbers[i] in rules[numbers[i + 1]]!!) {
+                        correct = false
+                        corrected = true
+                        numbers[i] = numbers[i+1].also { numbers[i + 1] = numbers[i] }
+                    }
                 }
             }
-            if (correct)
+
+            if (!corrected)
                 correctNumbers.add(numbers)
+            else
+                correctedNumbers.add(numbers)
+
+
         }
     }
 
-    val sum = correctNumbers.sumOf {
-        it[it.size / 2]
-    }
-
-    println("sum: $sum")
-
+    println("Solution part 01: ${correctNumbers.sumOf {it[it.size / 2] }}")
+    println("Solution part 02: ${correctedNumbers.sumOf {it[it.size / 2] }}")
 
 }
+
+
